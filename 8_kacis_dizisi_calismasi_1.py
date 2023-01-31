2e7c43cee6a0cd70adb5a238a50d6db55f4f0215
NECATKO <94390067+NECATKO@users.noreply.github.com>
2023-02-01 00:09:22 +0300

    kacis dizileri

---
+++ 8_kacis_dizisi_calismasi_1.py
@@ -0,0 +1,7 @@
+#Ters taksim (\)
+
+print('Yarin Antep\'e geliyorum.')
+
+print(" ")
+
+print("Antep'in bir asil ismi \"Gazianterp\" tir.")
---
+++ 8_kacis_dizisi_calismasi_2.py
@@ -0,0 +1,4 @@
+print("birinci satir\nikinci satir\nucuncu satir")
+print(" ")
+baslik = "Merhaba bu derse hos geldiniz!"
+print(baslik, "\n", "="*len(baslik), sep=(""))
